ce277ae84b3bfc2aaf34ef06a44db1bb7a2515cc
YieldRay <565332016@qq.com>
2021-12-04 23:56:13 +0800

    allow to redirect file

--- src/staticFileHosting.js
+++ src/staticFileHosting.js
@@ -1,67 +1,75 @@
 import http from "http";
-import { readFile, access } from "fs/promises";
-import { constants } from "fs";
+import { readFile, stat } from "fs/promises";
 
 function prependSlash(str) {
     if (str.startsWith("/")) return str;
     else return "/" + str;
 }
 
-function appendSlash(str) {
-    if (str.endsWith("/")) return str;
-    else return str + "/";
-}
 async function isFileAccessible(targetFile) {
     try {
-        await access(targetFile, constants.R_OK);
+        return (await stat(targetFile)).isFile();
     } catch {
         return false;
     }
-    return true;
 }
 
 // {public: "/", local: "./public/"} match in order
 async function host(mapOption = [{ public: "/", local: "./" }], port = 8080) {
-    const server = http.createServer((req, res) => {
+    if (typeof port !== "number") throw new Error("port should be a number!");
+    const server = http.createServer(async (req, res) => {
+        let isSuccess = false;
         console.log("> ", req.method, req.url);
-        mapOption.forEach(async (e) => {
+        for (const e of mapOption) {
+            let isSkip = false;
             // add slash
+            let targetFile = "";
+            let bare = "";
             e.public = prependSlash(e.public);
-            e.public = appendSlash(e.public);
-            e.local = appendSlash(e.local);
 
             // handle the request
-            if (req.url.startsWith(e.public)) {
-                let bare = req.url.replace(e.public, "");
-                let targetFile;
+            if (e.public.endsWith("/")) {
+                // for req folder
+                if (req.url.startsWith(e.public))
+                    bare = req.url.replace(e.public, "");
+                else isSkip = true;
+
                 if (bare.length === 0) {
                     // if endsWith '/', redirect to 'index.html', no fallback
-                    bare = "index.html";
-                }
-                targetFile = e.local + bare;
-                console.log(`   target: ${targetFile}`);
-
-                // check if file is accessable
-                if (await isFileAccessible(targetFile)) {
-                    // successfully read file
-                    const data = await readFile(targetFile);
-                    res.writeHead(200);
-                    res.end(data);
-                } else {
-                    return;
+                    if (e.local.endsWith("/")) bare = "index.html";
                 }
             }
-        });
-        // cannot read file, return 404
-        res.setHeader("content-type", "text/html");
-        res.writeHead(404);
-        res.end(`<html>
+            targetFile = e.local + bare;
+            if (isSkip)
+                console.log(`   skip: public=${e.public} local=${e.local}`);
+            else
+                console.log(
+                    `   try to access: target=${targetFile} public=${e.public} local=${e.local}`
+                );
+
+            // check if file is accessable
+            if (!isSkip && (await isFileAccessible(targetFile))) {
+                // successfully read file
+                // stdout
+                console.log(`   target: ${targetFile}`);
+                const data = await readFile(targetFile);
+                res.writeHead(200);
+                res.end(data);
+                isSuccess = true;
+            }
+        }
+        // cannot read file, return
+        if (!isSuccess) {
+            res.setHeader("content-type", "text/html");
+            res.writeHead(404);
+            res.end(`<html>
         <head><title>404 Not Found</title></head>
         <body>
         <center><h1>404 Not Found</h1></center>
         <hr><center>node.js</center>
         </body>
         </html>`);
+        }
     });
 
     server.listen(port);
@@ -70,9 +78,13 @@ async function host(mapOption = [{ public: "/", local: "./" }], port = 8080) {
     );
 }
 
-// host([{ public: "/", local: "./src/" }]);
-// or  host([{ public: "/", local: "./src" }]);
-// or just  host([{ public: "", local: "./src" }]);
+// host([
+//     { public: "/", local: "./src/" },
+//     { public: "/test.js", local: "./js/test.js" },
+// ]);
 
-// host([{ public: "/images", local: "./picture" }]);
+// CANNOT USE  host([{ public: "/", local: "./src" }]);
+// nut you can  host([{ public: "", local: "./src/" }]);
+
+// host([{ public: "/images/", local: "./picture/" }]);
 export default host;
--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 import { host, ls, runCommands } from "./index.js";
-ls()
-    .then(console.log)
-    .then(() => runCommands("ls"))
-    .then(() => host([{ public: "/", local: "./src" }]));
+// ls()
+//     .then(console.log)
+//     .then(() => runCommands("ls"));
+
+host([
+    { public: "a.js", local: "/" },
+    { public: "/", local: "./public/" },
+    {
+        public: "/test/",
+        local: "./test.js",
+    },
+]);
